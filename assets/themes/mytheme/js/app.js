a9910c10e4cda8b3c36295479f70298a98c96cc3
lthoang <lthoang.apcs.vn>
2016-05-20 00:03:40 +0700

    add feature support search tieng viet khong dau

--- assets/themes/mytheme/js/app.js
+++ assets/themes/mytheme/js/app.js
@@ -13,12 +13,43 @@ var getUrlParameter = function getUrlParameter(sParam) {
   }
 };
 
+var vietnameseToLatinCharacter = function (string) {
+  return string
+      .replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a")
+      .replace(/À|Á|Ả|Ã|Ạ|Ă|Ằ|Ắ|Ẳ|Ẵ|Ặ|Â|Ầ|Ấ|Ẩ|Ẫ|Ậ/g, "A")
+      .replace(/è|é|ẻ|ẽ|ẹ|ê|ề|ế|ể|ễ|ệ/g, "e")
+      .replace(/È|É|Ẻ|Ẽ|Ẹ|Ê|Ề|Ế|Ể|Ễ|Ệ/g, "E")
+      .replace(/ì|í|ỉ|ĩ|ị/g, "i")
+      .replace(/Ì|Í|Ỉ|Ĩ|Ị/g, "I")
+      .replace(/ò|ó|ỏ|õ|ọ|ô|ồ|ố|ổ|ỗ|ộ|ơ|ờ|ớ|ở|ỡ|ợ/g, "o")
+      .replace(/Ò|Ó|Ỏ|Õ|Ọ|Ô|Ồ|Ố|Ổ|Ỗ|Ộ|Ơ|Ờ|Ớ|Ở|Ỡ|Ợ/g, "O")
+      .replace(/ù|ú|ủ|ũ|ụ|ư|ừ|ứ|ử|ữ|ự/g, "u")
+      .replace(/Ù|Ú|Ủ|Ũ|Ụ|Ư|Ừ|Ứ|Ử|Ữ|Ự/g, "U")
+      .replace(/ỳ|ý|ỷ|ỹ|ỵ/g, "y")
+      .replace(/Ỳ|Ý|Ỷ|Ỹ|Ỵ/g, "Y")
+      .replace(/đ/g, "d")
+      .replace(/Đ/g, "D");
+}
+
+var specialCharacterToBlankSpace = function (string) {
+  //TODO: Add more special character to set
+  return string
+      .replace(/,|.\/|>|<|\?|;|:|'|"|`|~|!|@|#|$|%|^|&|\*|\(|\)|_|-|=|\+|[|]|\\/g, " ");
+}
+
+var removeDuplicateWhiteSpaces = function (string) {
+  return string.replace(/ +/g, " ");
+}
+
 $('document').ready(function () {
   var searchQuery = getUrlParameter('search');
   if (searchQuery) {
-    searchQuery = searchQuery.replace(/\+/g, ' ');
-    $('#search-form-jplist').val(searchQuery);
     $('#search-box').val(searchQuery);
+    searchQuery = vietnameseToLatinCharacter(
+        removeDuplicateWhiteSpaces(
+            specialCharacterToBlankSpace(searchQuery).trim()));
+
+    $('#search-form-jplist').val(searchQuery);
     $('.search-query').text("'" + searchQuery.toString() + "'");
   }
   $('#demo').jplist({
