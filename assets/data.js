34ce5c4d618ec7e5b60f784384d1dd550ca88cb0
Hoàng Lê <trhoanglee@Hoangs-MBP.local>
2016-06-16 22:43:07 +0700

    add autokey pictures

---
+++ assets/data.js
@@ -0,0 +1,42 @@
+/**
+ * Created by trhoanglee on 6/2/16.
+ */
+var getUrlParameter = function getUrlParameter(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
+    }
+  }
+};
+
+var searchProduct = function (query) {
+  //filter();
+  //sort();
+  //paging();
+  $.getJSON("/data/products.json", function(data) {
+    $.each(data, function (key, value) {
+      $('#result').append("<p>" + data[key].name + query + "</p>")
+    });
+  });
+}
+
+$('document').ready(function () {
+/*  var searchQuery = getUrlParameter('q');
+  if (searchQuery) {
+    searchQuery = searchQuery.replace(/\+/g, " ");
+    $('#search-box').val(searchQuery);
+    searchProduct(searchQuery);
+  }*/
+  $.getJSON("/data/autokey.json", function (data) {
+    $.each(data, function (key, value) {
+        $('#result').append("<p>" + data[key].name + "</p>");
+    });
+  });
+});
